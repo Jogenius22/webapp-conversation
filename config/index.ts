581e963f1853b6948032d253952c75998f72d7bd
Jogenius22 <108581854+Jogenius22@users.noreply.github.com>
2023-06-14 15:56:06 +0100

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,17 +1,17 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = ''
-export const API_KEY = ''
+export const APP_ID = '5e8d0c57-8da8-4466-9bbf-bebc202de6c8'
+export const API_KEY = 'app-aNZDgpJqDCyqU7RPmBiDje3M'
 export const API_URL = ''
 export const APP_INFO: AppInfo = {
   title: 'Chat APP',
   description: '',
   copyright: '',
   privacy_policy: '',
-  default_language: 'zh-Hans',
+  default_language: 'en',
 }
 
 export const isShowPrompt = false
-export const promptTemplate = 'I want you to act as a javascript console.'
+export const promptTemplate = ''
 
 export const API_PREFIX = '/api'
 
